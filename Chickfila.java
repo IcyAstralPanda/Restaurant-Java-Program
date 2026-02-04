b76acd7aaa753331fb751046202f7cbf0a0015c2
IcyAstralPanda <jaydnug@gmail.com>
2026-02-04 12:26:45 -0700

    Add files via upload

---
+++ Chickfila.java
@@ -0,0 +1,21 @@
+public class Chickfila {
+    // Properties
+    int waitTime;
+    double rating;
+    double avgPrice;
+    double distance;
+    String hoursOpen;
+
+    // Constructor
+    public Chickfila() {}
+
+    public Chickfila(int waitTime, double rating, double avgPrice, double distance, String hoursOpen) {
+        this.waitTime = waitTime;
+        this.rating = rating;
+        this.avgPrice = avgPrice;
+        this.distance = distance;
+        this.hoursOpen = hoursOpen;
+    }
+
+
+}
---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Use the Restaurant object
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
+
+
+
